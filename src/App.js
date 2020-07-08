b4a2302d62663e9aaa8cf924e07d868a7dfbb548
Jeff Sauer <cjsauer18@gmail.com>
2020-07-08 15:13:53 -0400

    deploy app to ghpages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-// import Navbar from "./node/Navbar.jsx"
 import SortingVisualizer from "./node/SortingVisualizer.jsx";
 import Toolbar from "./node/Toolbar.jsx";
 function App() {
