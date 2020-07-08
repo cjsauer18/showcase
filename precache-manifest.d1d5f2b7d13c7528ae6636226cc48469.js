82246a0cf60c51c3f2d0e3cbce001de264c3d97e
Jeff Sauer <cjsauer18@gmail.com>
2020-07-08 15:29:53 -0400

    Updates

---
+++ precache-manifest.d1d5f2b7d13c7528ae6636226cc48469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06b411d5b8ef3c6e27574bb0f4ffa74",
+    "url": "/showcase/index.html"
+  },
+  {
+    "revision": "d488f431f33da4a6b678",
+    "url": "/showcase/static/css/main.49db6c59.chunk.css"
+  },
+  {
+    "revision": "a3d791dbfb9143aae4d8",
+    "url": "/showcase/static/js/2.b61b5e3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/showcase/static/js/2.b61b5e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d488f431f33da4a6b678",
+    "url": "/showcase/static/js/main.2f92ff5c.chunk.js"
+  },
+  {
+    "revision": "85ba9f8b441c04720eb2",
+    "url": "/showcase/static/js/runtime-main.039bf16c.js"
+  }
+]);
